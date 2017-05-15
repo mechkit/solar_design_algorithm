81511ade2c1e9a16a5e79a672dec2ade7d1e9b10
Keith Showalter <kshowalter@fsec.ucf.edu>
2017-05-15 11:35:13 -0400

    Reorganized file structure.

--- make.sh
+++ make.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-node node_modules/process_code_defining_document/index.js SDA.md
-node node_modules/process_code_defining_document/index.js tables.md
+node node_modules/process_code_defining_document/index.js standard
